d48fde3b3ddf5e992455523c6567c245353c1f1f
Jacob Kuzmits <kuzmits@gmail.com>
2025-10-18 11:06:15 -0400

    fix: test class name was wrong

--- src/utils/tests/test_split_nodes_delimiter.py
+++ src/utils/tests/test_split_nodes_delimiter.py
@@ -1,58 +1,59 @@
-
 import unittest
 from utils.split_nodes_delimiter import split_nodes_delimiter
 from nodes.textnode import TextNode, TextType
 
-class TestHTMLNode(unittest.TestCase):
 
-	def test_inner_bold(self):
-		node = TextNode("This is text with a **bold** word", TextType.TEXT)
-		new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-		self.assertEqual(new_nodes[0].text, "This is text with a ")
-		self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
-		self.assertEqual(new_nodes[1].text, "bold")
-		self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
-		self.assertEqual(new_nodes[2].text, " word")
-		self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+class TestSplitNodesDelimiter(unittest.TestCase):
 
-	def test_double_inner_bold(self):
-		node = TextNode("This is **text** with a **bold** word", TextType.TEXT)
-		new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-		self.assertEqual(new_nodes[0].text, "This is ")
-		self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
-		self.assertEqual(new_nodes[1].text, "text")
-		self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
-		self.assertEqual(new_nodes[2].text, " with a ")
-		self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
-		self.assertEqual(new_nodes[3].text, "bold")
-		self.assertEqual(new_nodes[3].text_type, TextType.BOLD)
-		self.assertEqual(new_nodes[4].text, " word")
-		self.assertEqual(new_nodes[4].text_type, TextType.TEXT)
+    def test_inner_bold(self):
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "bold")
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
 
-	def test_inner_italic(self):
-		node = TextNode("This is text with an _italic_ word", TextType.TEXT)
-		new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
-		self.assertEqual(new_nodes[0].text, "This is text with an ")
-		self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
-		self.assertEqual(new_nodes[1].text, "italic")
-		self.assertEqual(new_nodes[1].text_type, TextType.ITALIC)
-		self.assertEqual(new_nodes[2].text, " word")
-		self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+    def test_double_inner_bold(self):
+        node = TextNode("This is **text** with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes[0].text, "This is ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "text")
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, " with a ")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[3].text, "bold")
+        self.assertEqual(new_nodes[3].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[4].text, " word")
+        self.assertEqual(new_nodes[4].text_type, TextType.TEXT)
 
-	def test_inner_code(self):
-		node = TextNode("This is text with a `code` word", TextType.TEXT)
-		new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-		self.assertEqual(new_nodes[0].text, "This is text with a ")
-		self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
-		self.assertEqual(new_nodes[1].text, "code")
-		self.assertEqual(new_nodes[1].text_type, TextType.CODE)
-		self.assertEqual(new_nodes[2].text, " word")
-		self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+    def test_inner_italic(self):
+        node = TextNode("This is text with an _italic_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(new_nodes[0].text, "This is text with an ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "italic")
+        self.assertEqual(new_nodes[1].text_type, TextType.ITALIC)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
 
-	def test_unmatched_bold(self):
-		node = TextNode("This is text with a **unmatched bold", TextType.TEXT)
-		with self.assertRaises(ValueError):
-			split_nodes_delimiter([node], "**", TextType.BOLD)
+    def test_inner_code(self):
+        node = TextNode("This is text with a `code` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "code")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_unmatched_bold(self):
+        node = TextNode("This is text with a **unmatched bold", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+
 
 if __name__ == "__main__":
     unittest.main()
