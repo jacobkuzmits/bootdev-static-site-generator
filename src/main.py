b4d1eb529a68069ff0fedeab2f249c908adf53af
Jacob Kuzmits <kuzmits@gmail.com>
2025-10-18 23:09:05 -0400

    feat: copy static/ to public/ at runtime

--- src/main.py
+++ src/main.py
@@ -1,9 +1,41 @@
-from textnode import TextNode
-from textnode import TextType
+import os
+import shutil
+
+
+def copy_static_to_public():
+    # delete public/
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+        print("Deleted public/")
+
+    # create public/
+    os.mkdir("public")
+
+    # copy static/ into public/
+    copy_directory("static", "public")
+
+
+def copy_directory(src, target):
+    src_path = os.path.abspath(os.path.join(".", src))
+    target_path = os.path.abspath(os.path.join(".", target))
+    src_contents = os.listdir(src_path)
+    for item in src_contents:
+        item_path = os.path.join(src_path, item)
+        is_dir = os.path.isdir(item_path)
+        if not is_dir:
+            target_file_path = os.path.join(target_path, item)
+            shutil.copy(item_path, target_file_path)
+            print(f"copied {item_path} to {target_file_path}")
+        else:
+            target_dir_path = os.path.join(target_path, item)
+            os.mkdir(target_dir_path)
+            print(f"created directory {target_dir_path}")
+            copy_directory(item_path, target_dir_path)
+
 
 def main():
-	node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-	print(node)
+    copy_static_to_public()
+
 
 if __name__ == "__main__":
-	main()
+    main()
